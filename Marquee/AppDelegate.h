ac75ac758dd2cc770849338141bfee696a6eb222
wanhouhong <wanhouhong@wanhouhongdemacbook-pro.local>
2017-09-20 14:49:34 +0800

    Initial Commit

---
+++ Marquee/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Marquee
+//
+//  Created by wanhouhong on 2017/9/20.
+//  Copyright © 2017年 wanhouhong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
